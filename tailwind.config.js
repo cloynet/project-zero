cab91a9297dab67293600c0867308f02d9aabf22
cloynet <eerenn327@hotmail.com>
2025-01-16 18:49:07 +0300

    guncel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+      }
+    },
+    extend: {
+      fontFamily: {
+        gemumu: ["Gemunu Libre", "serif"],
+        open: ["Open Sans", "serif"]
+      },
+      colors: {
+        'zero-red': "#BC1A45",
+        "zero-melon": "#FFD369",
+        "zero-grey": "#DDDDDD",
+        "zero-white": "#F7F7F7"
+      },
+      spacing: {
+        '128': '32rem'
+      }
+    },
   },
   plugins: [],
-}
+};
 
